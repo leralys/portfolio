95d6698aa6efe0bc40137b4256d782ba06e9e7fc
leralys <valerialysko@gmail.com>
2022-03-15 17:17:39 +0200

    animations

--- src/components/technologies/Technologies.jsx
+++ src/components/technologies/Technologies.jsx
@@ -1,5 +1,8 @@
-import { useMemo } from "react";
-import "./technologies.scss";
+import { useRef } from "react";
+import { useInViewport } from "react-in-viewport";
+
+import { fadeIn } from "../../animations/fadeIn";
+
 import css from "../../assets/images/technologies/css.png";
 import html from "../../assets/images/technologies/html.png";
 import js from "../../assets/images/technologies/js.png";
@@ -10,34 +13,21 @@ import sass from "../../assets/images/technologies/sass.png";
 import nodejs from "../../assets/images/technologies/nodejs.png";
 import express from "../../assets/images/technologies/express.png";
 
+import "./technologies.scss";
+
 const tech = [css, html, sass, js, react, redux, postgres, nodejs, express];
 
-const Technologies = () => {
-    const root = useMemo(() => document.querySelector(":root"), []);
-    const parallaxHandler = e => {
-        const x = (e.clientX - window.innerWidth / 2) / 150;
-        const y = (e.clientY - window.innerHeight / 2) / 100;
-        root.style.setProperty('--posX', -x);
-        root.style.setProperty('--posY', -y);
-    }
-    const parallaxTouchHandler = e => {
-        const x = (e.touches[0].clientX - window.innerWidth / 2) / 100;
-        const y = (e.touches[0].clientY - window.innerHeight / 2) / 100;
-        root.style.setProperty('--posX', -x);
-        root.style.setProperty('--posY', -y);
-    }
-    const resetParallax = e => {
-        root.style.setProperty('--posX', 0);
-        root.style.setProperty('--posY', 0);
+const Technologies = ({ menuOpen }) => {
+    const techFadeIn = useRef();
+    const { inViewport } = useInViewport(techFadeIn);
+    if (inViewport && !menuOpen) {
+        fadeIn(".fadeIn");
     }
     return (
-        <div className="technologies" id="technologies"
-            onMouseMove={parallaxHandler}
-            onTouchMove={parallaxTouchHandler}
-            onTouchEnd={resetParallax}>
-            <h1>Technologies that I use</h1>
-            <div className="container">
-                <div className="grid">
+        <div className="technologies" id="technologies">
+            <h1 className="fadeIn">Technologies that I use</h1>
+            <div className="container fadeIn">
+                <div className="grid" ref={techFadeIn}>
                     {tech.map((el, i) => (
                         <div className="item" key={i}>
                             <img src={tech[i]} alt={`${tech[i]}`} />
